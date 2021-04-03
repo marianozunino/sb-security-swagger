0a19a62ce049a074a5de49789f7b349b78a5cd82
Mariano Z <marianoz@zoho.com>
2021-04-03 15:02:48 -0300

    chore: remove unusued code

--- src/main/java/com/example/demo/dto/ApiValidationError.java
+++ src/main/java/com/example/demo/dto/ApiValidationError.java
@@ -2,11 +2,11 @@ package com.example.demo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
-@EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiValidationError {
 
   private String field;
--- src/main/java/com/example/demo/exception/ApiException.java
+++ src/main/java/com/example/demo/exception/ApiException.java
@@ -11,10 +11,6 @@ public class ApiException extends Exception {
   private HttpStatus status;
   private String error;
 
-  ApiException(HttpStatus status) {
-    this.status = status;
-  }
-
   public ApiException(HttpStatus status, String error) {
     this.status = status;
     this.error = error;
